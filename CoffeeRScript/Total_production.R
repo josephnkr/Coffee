e4d154e367c8b90aa3b8d75f673cdf4e881ffffc
Joseph Rodrigo <josephnkr@gmail.com>
2017-05-29 10:45:20 -0500

    Preparing data for scripting

---
+++ CoffeeRScript/Total_production.R
@@ -0,0 +1,10 @@
+#Github coffee project
+#Coffee production
+
+total.production <- data.frame(read.csv("production.txt"))
+total.production
+is.data.frame(total.production)
+head(total.production)
+str(total.production)
+colnames(total.production)
+rownames(total.production)
